2a0aee8408a8baedbdc207204296f675fb6c5532
rodtsampson <rodtsampson@yahoo.com>
2019-10-29 23:12:12 -0500

    Through Overloading Operators...afteradding the subtraction operator overload.

--- HelloWorld/Program_OverloadingOperators.cs
+++ HelloWorld/Program_OverloadingOperators.cs
@@ -20,6 +20,11 @@ namespace OverloadingOperators
             //new vector sums the x comp, y comp, z comp
             return new Vector(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
         }
+        public static Vector operator -(Vector vec1, Vector vec2)
+        {
+            //new vector subtracts the x comp, y comp, z comp
+            return new Vector(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+        }
         public override string ToString()//needed to printing a vector produces a good looking result
         {
             return "<" + x + "," + y + "," + z + ">";
@@ -34,7 +39,13 @@ namespace OverloadingOperators
             //make a second vector
             Vector v2 = new Vector(4, 5, 6);
             Vector sum = v1 + v2;//+operator is invoked
+            //make a third vector
+            Vector v3 = new Vector(1, 2, 3);
+            //make a fourth vector
+            Vector v4 = new Vector(4, 5, 6);
+            Vector diff = v4 - v3;//+operator is invoked
             System.Console.WriteLine("Sum is " + sum.ToString());
+            System.Console.WriteLine("Diff is " + diff.ToString());
         }
     }
 }
